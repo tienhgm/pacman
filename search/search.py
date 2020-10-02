a3cdcce1801aa43dfa3b96b16528003438f34ead
tienhgm <47660593+tienhgm@users.noreply.github.com>
2020-10-02 21:46:39 +0700

    1

--- search/search.py
+++ search/search.py
@@ -167,7 +167,29 @@ def nullHeuristic(state, problem=None):
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+     #util.raiseNotDefined()
+    visited = set()
+    p_queue = util.PriorityQueue()
+    p_queue.push((problem.getStartState(), []), 0)
+
+    while not p_queue.isEmpty():
+        state, actions = p_queue.pop()
+
+        if state in visited:
+            continue
+
+        visited.add(state)
+
+        if problem.isGoalState(state):
+            return actions
+
+        for successor, action, stepCost in problem.getSuccessors(state):
+            if successor not in visited:
+                p_queue.push(
+                    (successor, actions + [action]),
+                    stepCost + problem.getCostOfActions(actions) +
+                    heuristic(successor, problem = problem))
+
 
 
 # Abbreviations
